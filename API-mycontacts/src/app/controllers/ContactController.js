96f0f71bb34e29a0ef15599d622c33c17815b4aa
KesslerBarreto <kesslergremio@gmail.com>
2025-01-03 19:51:37 -0300

    Conectando com o banco de dados Postgres

--- API-mycontacts/src/app/controllers/ContactController.js
+++ API-mycontacts/src/app/controllers/ContactController.js
@@ -1,9 +1,9 @@
 const ContactsRepository = require('../repositories/ContactsRepository');
-const contactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
    async index(request, response) {
-        const contacts = await ContactsRepository.findAll();
+        const {orderBy} = request.query;
+        const contacts = await ContactsRepository.findAll(orderBy);
 
         response.json(contacts);
     }
@@ -68,12 +68,6 @@ class ContactController {
 
     async delete(request, response) {
         const { id } = request.params;
-
-        const contact = await ContactsRepository.findById(id);
-
-        if (!contact) {
-            return response.status(404).json({ error: 'User not found' });
-        }
 
         await ContactsRepository.delete(id);
         response.sendStatus(204);  //204: No content
